05d06e4d372333d4848122e6cbdb7943ae6aef9a
Sebastian <Sebastian@example.com>
2020-09-08 12:04:36 +0200

    pierwszy commit

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const nr = prompt("Podaj jakąś liczbę");
+
+if (nr > 20) { //jeżeli...
+    console.log("Liczba " + nr + " jest większa od 20");
+} else { //w przeciwnym razie...
+    console.log("Liczba jest mniejsza od 20");
+}
