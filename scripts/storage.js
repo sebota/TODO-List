7d23d1172264ec5390427163c92778d65be88dd1
Sebastian <Sebastian@example.com>
2020-09-12 17:16:50 +0200

    nic

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,12 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const todoList = document.querySelector("#todoList");
-    const todoForm = document.querySelector("#todoForm");
+    //pobieram elementy
+    const form = document.querySelector("#todoForm");
+    const textarea = document.querySelector("#todoMessage");
+    const list =  document.querySelector("#todoText")
 
-    todoForm.addEventListener("submit", e => {
-        localStorage.setItem("todolistValue", todoList.value);
+    //jeżeli jest element w localStorage, ustawiam wartość dla textarea
+    if (localStorage.getItem("textareaValue") !== null) {
+        textarea.value = localStorage.getItem("textareaValue");
+    }
+
+    //podczas wpisywania do textarea aktualizuję localStorage
+    textarea.addEventListener("input", e => {
+        localStorage.setItem("textareaValue", textarea.value);
+        localStorage.setItem("todolistValue", textarea.value);
+    });
+
+    form.addEventListener("submit", e => {
+        //gdy użytkownik wysyła formularz, zakładamy że już skończył pisać
+        //więc nie ma co trzymać treści w localStorage
+        localStorage.removeItem("textareaValue");
     });
 
     if (localStorage.getItem("todolistValue") !== null) {
-        todoList.value = localStorage.getItem("todolistValue");
+        list.value = localStorage.getItem("todolistValue");
     }
 });
