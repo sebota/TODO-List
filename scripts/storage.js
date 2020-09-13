ba9318643cd3d0f63b00bf16cd38ff7af3c623f8
Sebastian <Sebastian@example.com>
2020-09-13 20:02:15 +0200

    storage

--- scripts/storage.js
+++ scripts/storage.js
@@ -2,7 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
     //pobieram elementy
     const form = document.querySelector("#todoForm");
     const textarea = document.querySelector("#todoMessage");
-    const list =  document.querySelector("#todoText")
+    var tab = [];
+    var tab_date = [];
+
+    function addtaskStorage(text, date) {
+        const element = document.createElement("div");
+        element.classList.add("element");
+
+        //pobieram zawartość templatki
+        const elementInner = document.querySelector("#elementTemplate").content.cloneNode(true);
+
+        //wrzucam do elementu
+        element.append(elementInner);
+
+        //tworzę datę
+        element.querySelector(".element-date").innerText = date;
+
+        //wstawiam tekst
+        element.querySelector(".element-text").innerText = text;
+
+        //i wrzucam element do listy
+        todoList.append(element);
+    }
 
     //jeżeli jest element w localStorage, ustawiam wartość dla textarea
     if (localStorage.getItem("textareaValue") !== null) {
@@ -12,16 +33,36 @@ document.addEventListener("DOMContentLoaded", () => {
     //podczas wpisywania do textarea aktualizuję localStorage
     textarea.addEventListener("input", e => {
         localStorage.setItem("textareaValue", textarea.value);
-        localStorage.setItem("todolistValue", textarea.value);
+        // var temp = textarea.value;
     });
 
     form.addEventListener("submit", e => {
         //gdy użytkownik wysyła formularz, zakładamy że już skończył pisać
         //więc nie ma co trzymać treści w localStorage
+        tab.push(localStorage.getItem("textareaValue"));
+        localStorage["table"] = JSON.stringify(tab);
+
+        localStorage.setItem("todolistValue", localStorage.getItem("textareaValue"));
         localStorage.removeItem("textareaValue");
+
+        const date = new Date();
+        const dateText = `${date.getDate()} - ${date.getMonth()+1} - ${date.getFullYear()} godz.: ${date.getHours()}:${date.getMinutes()}`;
+        tab_date.push(toString(dateText));
+        localStorage["table_date"] = JSON.stringify(tab_date);
     });
 
-    if (localStorage.getItem("todolistValue") !== null) {
-        list.value = localStorage.getItem("todolistValue");
+    if (localStorage["table"] !== null) { 
+        tableparse = JSON.parse(localStorage["table"]);
+        tableparsedate = JSON.parse(localStorage["table_date"]);
+        for (const [i] of tableparse.entries()) {
+            addtaskStorage(tableparse[i], tableparsedate[i]);
+        }
     }
+
+    todoList.addEventListener("click", e => {
+        if (e.target.classList.contains("element-delete")) {
+            localStorage.removeItem("todolistValue");
+            localStorage.removeItem("todolistDate");
+        }
+    });
 });
